a134f88ae6b9d4179988968a5829beeb076cdbe2
Abdurrahman <dreydare16@gmail.com>
2023-12-02 21:31:04 +0100

    adujusted font size and bar size

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,9 @@
 export default function Nav() {
     return (
         <nav className="p-4 flex justify-between items-center">
-            <h1 className="logo text-lg">SwiftCart</h1>
+            <h1 className="logo text-2xl">SwiftCart</h1>
 
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
             </svg>
         </nav>
