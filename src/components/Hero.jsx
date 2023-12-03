404c7bcd3a36f6f59a823dd6e61751cd3ae955c6
Abdurrahman <dreydare16@gmail.com>
2023-12-03 10:29:37 +0100

    button created

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,14 +2,17 @@ import heroBag from "../assets/hero-bag.png";
 
 export default function Hero(){
     return (
-        <header>
-            <figure className="w-11/12 max-w-xl mx-auto mt-8 rounded-md bg-hero-cream flex justify-between items-center p-5 pr-0">
+        <header className="px-5">
+            <figure className="max-w-xl mx-auto mt-8 mb-10 rounded-md bg-hero-cream flex justify-between items-center p-5 pr-0">
                 <div>
                     <h1 className="text-hero-heading text-xl font-bold mb-2">Discover Unique Products Online</h1>
                     <p className="text-hero-desc text-sm">Your One-Stop Shop for Quality Goods.</p>
                 </div>
                 <img src={heroBag} alt="shopping-bags" className="w-40"/>
             </figure>
+
+            <p className="text-gray-800 leading-normal m-auto text-sm">Welcome to SwiftCart, where passion meets product. Our journey began with a vision to redefine the way you shop. Dive into a world of carefully curated items that reflect our commitment to quality, style, and innovation.</p>
+             <button className="mt-6 bg-hero-cream text-hero-desc px-3 py-2 text-sm rounded">Shop now &rarr;</button>
         </header>
     )
 }
