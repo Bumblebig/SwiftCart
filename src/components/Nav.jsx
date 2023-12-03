c1a01f80a20d02bc118166c52b2f5b007464ae40
Abdurrahman <dreydare16@gmail.com>
2023-12-03 09:50:08 +0100

    adjusted logo and hamburger sizes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,9 @@
 export default function Nav() {
     return (
         <nav className="p-5 flex justify-between items-center">
-            <h1 className="logo text-2xl">SwiftCart</h1>
+            <h1 className="logo text-xl">SwiftCart</h1>
 
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-8">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
             </svg>
         </nav>
