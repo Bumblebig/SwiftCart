a0891a7de42e8bd8b176edb37fe67cd5ed965a6d
Abdurrahman <dreydare16@gmail.com>
2023-12-03 09:48:23 +0100

    adjusted card sizing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import heroBag from "../assets/hero-bag.png";
 export default function Hero(){
     return (
         <header>
-            <figure className="w-11/12 max-w-xl mx-auto mt-8 rounded-md bg-hero-cream flex justify-between items-center p-5">
+            <figure className="w-11/12 max-w-xl mx-auto mt-8 rounded-md bg-hero-cream flex justify-between items-center p-5 pr-0">
                 <div>
                     <h1 className="text-hero-heading text-xl font-bold mb-2">Discover Unique Products Online</h1>
                     <p className="text-hero-desc text-sm">Your One-Stop Shop for Quality Goods.</p>
