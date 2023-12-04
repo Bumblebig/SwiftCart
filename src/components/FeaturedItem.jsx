b948ad17773fc63bc5dfa5800623229a0facb208
Abdurrahman <dreydare16@gmail.com>
2023-12-04 18:30:55 +0100

    created skeleton for images before load

--- src/components/FeaturedItem.jsx
+++ src/components/FeaturedItem.jsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import prodImage from "../assets/brown-sandals.jpg";
 
 export default function FeaturedItem(){
+    const [isImageLoaded, setIsImageLoaded] = useState(false);
+    const handleImageLoad = function() {
+        setIsImageLoaded(true)
+    }
+
+    let errStyle = wrapperStyle = '';
+    const handleLoadError = function() {
+        errStyle = "display: block";
+        wrapperStyle = "display: flex; align-items: center; justify-content: center;";
+    }
+
+
     return(
         <figure className="w-3/6 box-content flex-shrink-0">
-            <img src={prodImage} alt="sandals" className="block w-full rounded-t-md" />
+            <div className={`block w-full rounded-t-md bg-gray-200 ${isImageLoaded ? '' : 'h-56'}`} style={wrapperStyle}>
+                <img src={prodImage} alt="sandals" className="block w-full h-auto rounded-t-md" onLoad={handleImageLoad} onError={handleLoadError} />
+                <p className="text-center text-sm text-gray-600 hidden" style={errStyle}>Image could not be loaded</p>
+            </div>
             <h3 className="price text-xl mt-6 px-2">₦5,000.00</h3>
             <p className="text-sm text-gray-700 mt-1 px-2 pb-7">Bucaneer Sandals</p>
         </figure>
