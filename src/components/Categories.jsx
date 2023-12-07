11893b0fd176294b918dd04fb7cebd7263ad83db
Abdurrahman <dreydare16@gmail.com>
2023-12-07 09:32:42 +0100

    dynamic category datas

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,53 @@
 import CategoryCard from './CategoryCard';
+import Sandals from '../assets/sandals.jpg';
+import Sneakers from '../assets/sneakers.jpg';
+import Slides from '../assets/slides.jpg';
+import Crocs from '../assets/crocs.jpg';
+import Flips from '../assets/flip-flops.jpg';
 
 export default function Categories() {
+    const data = [
+        {
+            id: 1,
+            imgURL: Sneakers,
+            desc: 'Sneakers',
+        },
+        {
+            id: 2,
+            imgURL: Sandals,
+            desc: 'Sandals',
+        },
+        {
+            id: 3,
+            imgURL: Slides,
+            desc: 'Slides',
+        },
+        {
+            id: 4,
+            imgURL: Crocs,
+            desc: 'Crocs',
+        },
+        {
+            id: 5,
+            imgURL: Flips,
+            desc: 'Flip-Flops',
+        },
+        
+    ];
+
+    const card = data.map((item) => {
+        return (<CategoryCard
+            key={item.id}
+            image={item.imgURL}
+            text={item.desc}
+        />)
+    })
+
     return (
         <section>
             <h2 className="mb-8">Categories</h2>
             <div className="m-auto flex flex-col items-center gap-4">
-                <CategoryCard />
-                <CategoryCard />
-                <CategoryCard />
-                <CategoryCard />
-                <CategoryCard />
+                {card}
             </div>
         </section>
     )
--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,16 +1,16 @@
 import Sneakers from '../assets/sneakers.jpg'
 
-export default function CategoryCard() {
+export default function CategoryCard({image, text}) {
     // const imgPath = 'sneakers.jpg'
     const style ={ 
-        backgroundImage: `linear-gradient(180deg, rgba(4,4,4,0.27494747899159666) 58%, rgba(4,4,4,0.6867121848739496) 88%), url("${Sneakers}")`,
+        backgroundImage: `linear-gradient(180deg, rgba(4,4,4,0.27494747899159666) 58%, rgba(4,4,4,0.6867121848739496) 88%), url("${image}")`,
         backgroundRepeat: 'no-repeat',
-        backgroundSize: 'contain'
+        backgroundSize: 'fit'
     }
     return (
         <figure className="w-11/12 h-56  flex flex-col items-center justify-end p-3" style={style}>
             <div>
-                <p className="text-white text-center text-base font-bold">Sneakers</p>
+                <p className="text-white text-center text-base font-bold">{text}</p>
                 <button className="mt-3 bg-hero-desc text-white px-3 py-2 text-sm">Shop now &rarr;</button>
             </div>
         </figure>
