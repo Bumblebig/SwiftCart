4e120f3e213f69f47e3529220480df23c0147344
Abdurrahman <dreydare16@gmail.com>
2023-12-08 04:18:34 +0100

    fixed caps error

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -24,7 +24,7 @@ export default function Steps(){
 
     return (
         <section>
-            <h2 className="mb-8">How it works</h2>
+            <h2 className="mb-8">How It works</h2>
             <div className="flex flex-col gap-8">
                 {steps}
             </div>
